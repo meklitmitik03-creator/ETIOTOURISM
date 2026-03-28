3528f3a87676e71e33723b5eb3636225ef01b56f
meklitmitik03-creator <meklitmitik03@gmail.com>
2026-03-28 12:48:30 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+function toggleSidebar(){
+  document.querySelector(".sidebar").classList.toggle("hide");
+}
+
+function toggleChat(){
+  let chat = document.getElementById("chatBox");
+  chat.style.display = chat.style.display === "flex" ? "none" : "flex";
+}
+
+function sendMessage(){
+  let input = document.getElementById("chatInput").value;
+  let body = document.getElementById("chatBody");
+
+  body.innerHTML += "<p><b>You:</b> "+input+"</p>";
+  body.innerHTML += "<p><b>AI:</b> Coming soon...</p>";
+
+  document.getElementById("chatInput").value="";
+}
